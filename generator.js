df552ca4b16d870d3daa4ac5a67874725bdf0e07
Jaime Pantoja <canjpo@gmail.com>
2024-03-04 08:54:41 -0800

    Content of the Message

---
+++ generator.js
@@ -0,0 +1,12 @@
+/** Create a list of the different elements that will create random messages **/
+// Greetings
+const greetings = ["Hello", "Hi", "Hey", "Howdy", "Greetings", "Yo", "Salutations", "What's up", "Good day", "Hi there", "Hola", "Bonjour", "Ciao", "Namaste", "Salaam", "Konichiwa", "G'day", "Aloha","Shalom", "Sup"];
+// Phrases 
+const motivationalPhrases = ["Keep coding, you're making progress!", "Every line of code you write brings you closer to your goal.", "Embrace the challenges, they make you a better coder.",
+"The more you code, the more you learn.", "Stay determined, success is just around the corner.",
+"Coding is a journey, enjoy the process.", "Believe in yourself, you're capable of great things.",
+"Mistakes are opportunities to learn and grow.", "You're not just writing code, you're building solutions.", "Celebrate your small victories, they add up to big accomplishments."];
+// Goodbyes
+const goodbyes = ["Goodbye", "Farewell", "See you later", "Bye", "Take care", "Adios", "So long", "Catch you later", "See you soon", "Have a great day"];
+
+
